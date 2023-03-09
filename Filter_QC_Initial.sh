146c3747858cecb6962700ec6a4849d3e79c63b7
MeganL314 <megantherese314@gmail.com>
2023-03-09 10:30:29 -0500

    Adjust initial QC script

--- Filter_QC_Initial.sh
+++ Filter_QC_Initial.sh
@@ -1,5 +1,6 @@
 #$ -cwd
 module load plink/1.90b6.6
+module load bcftools
 
 lifted_path=/dcs04/mathias/data/mlynch/ArrayData/Affymetrix/liftover
 geno=ROSMAP.Affy.b38
@@ -20,3 +21,30 @@ plink --bfile $geno_no_AT_CG \
       --maf 0.000001 --geno 0.05 --hwe 0.000001 \
       --mind 0.2 \
       --make-bed --out $geno_pre_qc
+
+
+
+#create vcf files
+plink --bfile $geno_pre_qc --recode vcf --out $geno_pre_qc
+
+
+#add 'chr' to vcf files
+awk '{ 
+        if($0 !~ /^#/) 
+            print "chr"$0;
+        else if(match($0,/(##contig=<ID=)(.*)/,m))
+            print m[1]"chr"m[2];
+        else print $0 
+      }' $geno_pre_qc.vcf > $geno_with_chr.vcf
+
+
+
+bcftools view ${geno}_with_chr.vcf -Oz -o ${geno}_with_chr.vcf.gz
+bcftools index ${geno}_with_chr.vcf.gz
+
+
+#split by chromosome
+bcftools index -s  ${geno}_with_chr.vcf.gz | cut -f 1 | while read C; do bcftools view -O z -o ${geno}.${C}.vcf.gz ${geno}_with_chr.vcf.gz "${C}" ; done
+
+
+
